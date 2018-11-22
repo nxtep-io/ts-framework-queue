b712b315d96e1becb5edd89a709f714f667221f8
Luis Brito <luiseduardo14@gmail.com>
2018-11-22 01:36:54 -0200

    Build: Fixing imports

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,5 +1,5 @@
-export * from './defaults';
-export { AMQPOptions, AMQPServiceOptions, default as default } from './AMQP';
+export { AMQPMessage, AMQPOptions, AMQPServiceOptions, default as default } from './AMQP';
 export { ChannelOptions, default as Channel } from './Channel';
+export * from './defaults';
 export { default as Exchange, ExchangeOptions, ExchangeSubscriber, QueueInformation } from './Exchange';
 export * from './utils';
